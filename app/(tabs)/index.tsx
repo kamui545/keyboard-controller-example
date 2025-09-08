b867c2eb97c9e7fbd0af4bb0a9f9bd1a8fee8a51
tian <istiancz@gmail.com>
2025-09-08 20:10:35 +0800

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,83 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, View } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { FlashList } from '@shopify/flash-list'
+import { KeyboardAvoidingView } from 'react-native-keyboard-controller'
+
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import React from 'react'
+
+const messages = [
+	'Hello, how are you?',
+	'I am fine, thank you!',
+	'What is your name?',
+	'My name is John Doe.',
+	'Hello, how are you?',
+	'I am fine, thank you!',
+	'What is your name?',
+	'My name is John Doe.',
+	'Hello, how are you?',
+	'I am fine, thank you!',
+	'What is your name?',
+	'My name is John Doe.',
+	'My name is John Doe.',
+	'My name is John Doe.',
+	'My name is John Doe.',
+	'My name is John Doe.',
+]
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<KeyboardAvoidingView
+				behavior={'translate-with-padding'}
+				style={{ flex: 1, paddingHorizontal: 12 }}
+			>
+				<FlashList
+					maintainVisibleContentPosition={{
+						autoscrollToBottomThreshold: 0.2,
+						animateAutoScrollToBottom: true,
+						startRenderingFromBottom: true,
+					}}
+					renderItem={({ item }) => (
+						<ThemedView style={styles.messageContainer}>
+							<ThemedText type='default'>{item}</ThemedText>
+						</ThemedView>
+					)}
+					data={messages}
+				/>
+				<View>
+					<TextInput placeholder='focus here' style={styles.input} />
+				</View>
+			</KeyboardAvoidingView>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+	titleContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 8,
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8,
+	},
+	messageContainer: {
+		padding: 16,
+		backgroundColor: 'white',
+		flexDirection: 'row',
+		flexShrink: 0,
+		flexGrow: 0,
+		width: '50%',
+		borderRadius: 8,
+		marginBottom: 8,
+	},
+	input: {
+		padding: 16,
+		backgroundColor: 'white',
+		borderRadius: 8,
+		marginBottom: 8,
+	},
+})
